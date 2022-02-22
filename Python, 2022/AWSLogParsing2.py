094ae516829673d3d711767f73153d33a0ce37b7
Kaan <71042922+YataganSabre@users.noreply.github.com>
2022-02-21 22:21:15 -0600

    Added comment at top that explains what i was supposed to find

--- Python, 2022/AWSLogParsing2.py
+++ Python, 2022/AWSLogParsing2.py
@@ -1,3 +1,9 @@
+#How many requests were made on each day? 
+#How many requests were made on a week-by-week basis? Per month?
+#What percentage of the requests were not successful (any 4xx status code)?
+#What percentage of the requests were redirected elsewhere (any 3xx codes)?
+#What was the most-requested file?
+#What was the least-requested file?
 import re
 import datetime as d
 import urllib.request
